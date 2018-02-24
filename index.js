09b9e1b164434615b4f6f0c9ff656c3f9460314c
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-24 00:35:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ this.name = name
 this.homeState = homeState
 this.training = training
 }
-BoardMember.prototype.
-//
-.veto = function(){
-return 'No, I must disagree'
+BoardMember.prototype..veto = function(){
+  return 'No, I must disagree'
 }
+//
+
 BoardMember.prototype.approve = function(){
 return 'You can do that!'
 }
@@ -22,4 +22,3 @@ BoardMember.prototype.releasePressStatement = function(){
 BoardMember.prototype.sayHi = function(){
 return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
-
